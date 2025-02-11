f2824083a3a1ebcc6be39798a10e247624a87b22
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-11 13:40:49 -0500

    Doing things for Problem One, may or may not go back to shorten if I figure out loops again

--- Program.cs
+++ Program.cs
@@ -8,12 +8,40 @@
             int diceOne, diceTwo;
             int minOne, minTwo, minThree, minFour, minFive;
             int maxOne, maxTwo, maxThree, maxFour, maxFive;
+            int rangeOne, rangeTwo, rangeThree, rangeFour, rangeFive;
             diceOne = generator.Next(1, 7);
             diceTwo = generator.Next(1, 7);
 
             //1. Random Integers
 
             Console.WriteLine("Please type down a minimum and maximum number for each range");
+            Console.WriteLine("Range One:");
+            minOne = Convert.ToInt32(Console.ReadLine());
+            maxOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Range Two:");
+            minTwo = Convert.ToInt32(Console.ReadLine());
+            maxTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Range Three:");
+            minThree = Convert.ToInt32(Console.ReadLine());
+            maxThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Range Four:");
+            minFour = Convert.ToInt32(Console.ReadLine());
+            maxFour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Range Five:");
+            minFive = Convert.ToInt32(Console.ReadLine());
+            maxFive = Convert.ToInt32(Console.ReadLine());
+
+            rangeOne = generator.Next(minOne, maxOne);
+            rangeTwo = generator.Next(minTwo, maxTwo);
+            rangeThree = generator.Next(minThree, maxThree);
+            rangeFour = generator.Next(minFour, maxFour);
+            rangeFive = generator.Next(minFive, maxFive);
+
+            Console.WriteLine(rangeOne);
+            Console.WriteLine(rangeTwo);
+            Console.WriteLine(rangeThree);
+            Console.WriteLine(rangeFour);
+            Console.WriteLine(rangeFive);
 
             //2. Dice Roller
 
